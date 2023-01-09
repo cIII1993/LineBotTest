e6f386a3e74081b26b6b15fa83d48886506e63e1
cIII1993 <jefferychen16@gmail.com>
2023-01-10 00:56:10 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -566,7 +566,7 @@ class Bot {
 		////week
 		week(inputStr) {
 			let returnStr = '基本骰組：';
-			let num = inputStr.split('\');
+			let num = inputStr.split('\\');
 			//let d = new Date(num[0], num[1]-1, num[2]);
 			//returnStr += d;
 			return returnStr;
